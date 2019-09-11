d5d7ff02c018e883ac70ef391c2b0ef0bf9d2ed3
Tony Rucker <tonyrucker@WTA-MBP615.local>
2019-09-11 13:58:16 -0400

    initial autho integration

--- src/App.js
+++ src/App.js
@@ -1,174 +1,94 @@
-// /client/App.js
+
 import React, { Component } from 'react';
-import axios from 'axios';
+import {
+  ReactiveBase,
+  CategorySearch,
+  RatingsFilter,
+  ResultCard
+} from '@appbaseio/reactivesearch';
 
 class App extends Component {
-  // initialize our state
-  state = {
-    data: [],
-    id: 0,
-    message: null,
-    intervalIsSet: false,
-    idToDelete: null,
-    idToUpdate: null,
-    objectToUpdate: null,
-  };
-
-  // when component mounts, first thing it does is fetch all existing data in our db
-  // then we incorporate a polling logic so that we can easily see if our db has
-  // changed and implement those changes into our UI
-  componentDidMount() {
-    this.getDataFromDb();
-    if (!this.state.intervalIsSet) {
-      let interval = setInterval(this.getDataFromDb, 1000);
-      this.setState({ intervalIsSet: interval });
-    }
+  onData(res) {
+    const result = {
+      image: "https://www.enterprise.com/content/dam/global-vehicle-images/cars/FORD_FOCU_2012-1.png",
+      title: res.name,
+      rating: res.rating,
+      desc: res.brand,
+      url: "#"
+    };
+    return result;
   }
 
-  // never let a process live forever
-  // always kill a process everytime we are done using it
-  componentWillUnmount() {
-    if (this.state.intervalIsSet) {
-      clearInterval(this.state.intervalIsSet);
-      this.setState({ intervalIsSet: null });
-    }
-  }
-
-  // just a note, here, in the front end, we use the id key of our data object
-  // in order to identify which we want to Update or delete.
-  // for our back end, we use the object id assigned by MongoDB to modify
-  // data base entries
-
-  // our first get method that uses our backend api to
-  // fetch data from our data base
-  getDataFromDb = () => {
-    fetch('http://localhost:3001/api/getData')
-      .then((data) => data.json())
-      .then((res) => this.setState({ data: res.data }));
-  };
-
-  // our put method that uses our backend api
-  // to create new query into our data base
-  putDataToDB = (title, message) => {
-    let currentIds = this.state.data.map((data) => data.id);
-    let idToBeAdded = 0;
-    while (currentIds.includes(idToBeAdded)) {
-      ++idToBeAdded;
-    }
-
-    axios.post('http://localhost:3001/api/putData', {
-      id: idToBeAdded,
-      title: title,
-      message: message,
-    });
-  };
-
-  // our delete method that uses our backend api
-  // to remove existing database information
-  deleteFromDB = (idTodelete) => {
-    parseInt(idTodelete);
-    let objIdToDelete = null;
-    this.state.data.forEach((dat) => {
-      if (dat.id == idTodelete) {
-        objIdToDelete = dat._id;
-      }
-    });
-
-    axios.delete('http://localhost:3001/api/deleteData', {
-      data: {
-        id: objIdToDelete,
-      },
-    });
-  };
-
-  // our update method that uses our backend api
-  // to overwrite existing data base information
-  updateDB = (idToUpdate, updateToApply) => {
-    let objIdToUpdate = null;
-    parseInt(idToUpdate);
-    this.state.data.forEach((dat) => {
-      if (dat.id == idToUpdate) {
-        objIdToUpdate = dat._id;
-      }
-    });
-
-    axios.post('http://localhost:3001/api/updateData', {
-      id: objIdToUpdate,
-      update: { message: updateToApply },
-    });
-  };
-
-  // here is our UI
-  // it is easy to understand their functions when you
-  // see them render into our screen
   render() {
-    const { data } = this.state;
     return (
-      <div>
-        <ul>
-          {data.length <= 0
-            ? 'NO DB ENTRIES YET'
-            : data.map((dat) => (
-                <li style={{ padding: '10px' }} key={data.title}>
-                  <span style={{ color: 'gray' }}> id: </span> {dat.id} <br />
-                  <span style={{ color: 'gray' }}> title: </span> {dat.title} <br />
-                  <span style={{ color: 'gray' }}> Post: </span>
-                  {dat.message}
-                </li>
-              ))}
-        </ul>
-        <div style={{ padding: '10px' }}>
-        <input
-            type="text"
-            onChange={(e) => this.setState({ title: e.target.value })}
-            placeholder="Title of Blog Post"
-            style={{ width: '200px' }}
-          />
-          <input
-            type="text"
-            onChange={(e) => this.setState({ message: e.target.value })}
-            placeholder="add something in the database"
-            style={{ width: '200px' }}
-          />
-          <button onClick={() => this.putDataToDB(this.state.title, this.state.message)}>
-            ADD
-          </button>
+      <ReactiveBase
+        app="car-store"
+        credentials="cf7QByt5e:d2d60548-82a9-43cc-8b40-93cbbe75c34c"
+      >
+        <div className="row reverse-labels">
+          <div className="col s6 col-xs-6">
+            <div className="row">
+              <div className="col s12 col-xs-12">
+                <CategorySearch
+                  componentId="SearchSensor"
+                  dataField="name"
+                  categoryField="brand.raw"
+                  placeholder="Search Cars"
+                />
+              </div>
+              <div className="col s12 col-xs-12">
+                <RatingsFilter
+                  componentId="RatingsSensor"
+                  dataField="rating"
+                  title="RatingsFilter"
+                  data={
+                    [{ start: 4, end: 5, label: "4 stars and up" },
+                    { start: 3, end: 5, label: "3 stars and up" },
+                    { start: 2, end: 5, label: "2 stars and up" },
+                    { start: 1, end: 5, label: "> 1 stars" }]
+                  }
+                  defaultSelected={{
+                      "start": 3,
+                      "end": 5
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="col s6 col-xs-6">
+            <ResultCard
+              componentId="SearchResult"
+              dataField="name"
+              title="Results"
+              from={0}
+              size={20}
+              onData={this.onData}
+              sortOptions={[
+                {
+                    label: "Lowest Price First",
+                    dataField: "price",
+                    sortBy: "asc"
+                },
+                {
+                    label: "Highest Price First",
+                    dataField: "price",
+                    sortBy: "desc"
+                },
+                {
+                    label: "Most rated",
+                    dataField: "rating",
+                    sortBy: "desc"
+                }
+              ]}
+              react={{
+                  and: ["SearchSensor", "RatingsSensor"]
+              }}
+            />
+          </div>
         </div>
-        <div style={{ padding: '10px' }}>
-          <input
-            type="text"
-            style={{ width: '200px' }}
-            onChange={(e) => this.setState({ idToDelete: e.target.value })}
-            placeholder="put id of item to delete here"
-          />
-          <button onClick={() => this.deleteFromDB(this.state.idToDelete)}>
-            DELETE
-          </button>
-        </div>
-        <div style={{ padding: '10px' }}>
-          <input
-            type="text"
-            style={{ width: '200px' }}
-            onChange={(e) => this.setState({ idToUpdate: e.target.value })}
-            placeholder="id of item to update here"
-          />
-          <input
-            type="text"
-            style={{ width: '200px' }}
-            onChange={(e) => this.setState({ updateToApply: e.target.value })}
-            placeholder="put new value of the item here"
-          />
-          <button
-            onClick={() =>
-              this.updateDB(this.state.idToUpdate, this.state.updateToApply)
-            }
-          >
-            UPDATE
-          </button>
-        </div>
-      </div>
-    );
+      </ReactiveBase>
+      );
+    }
   }
-}
 
-export default App;
+  export default App;
